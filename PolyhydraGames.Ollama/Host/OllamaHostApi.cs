960ae474f6bd6a0fbb7850b6c8c0f998952af983
chris <lancer1977@gmail.com>
2024-09-19 03:02:30 -0400

    api

--- PolyhydraGames.Ollama/Host/OllamaHostApi.cs
+++ PolyhydraGames.Ollama/Host/OllamaHostApi.cs
@@ -9,14 +9,14 @@ namespace PolyhydraGames.Ollama.Host
         public OllamaHostApi(IOllamaHostSiteConfig config)
         {
             Config = config;
-            _client.DefaultRequestHeaders.Add("streamid", Config.WebKey);
+            _client.DefaultRequestHeaders.Add("streamerid", Config.WebKey);
         }
 
         public IOllamaHostSiteConfig Config { get; set; }
 
         public async Task<bool> AddMessage(AddMessageParams message)
         { 
-            var endpoint = Config.Url + "/api/ai/AddMessage";
+            var endpoint = Config.Url + "/api/ai/addmessage";
             var content =  JsonContent.Create(message);
             var response = await _client.PostAsync(endpoint, content);
             var ok = response.IsSuccessStatusCode;

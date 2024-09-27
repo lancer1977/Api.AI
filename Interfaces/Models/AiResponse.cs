14e9113630d5e8ba81395295e61e1b2732f3e8b2
chris <lancer1977@gmail.com>
2024-09-27 16:54:59 -0400

    duh what!

--- Interfaces/Models/AiResponse.cs
+++ Interfaces/Models/AiResponse.cs
@@ -2,17 +2,6 @@
 
 namespace PolyhydraGames.AI.Models;
 
-public interface IServer
-{
-    string Name { get; set; }
-    string Description { get; set; }
-    List<string> Models { get; set; }
-    int Priority { get; set; }
-    string Availability { get; set; }
-    int Speed { get; set; }
-    string Address { get; set; }
-}
-
 public   static partial class AiResponse
 {
     public static AiResponseType<T> Create<T>(string rawResponse)
@@ -20,21 +9,7 @@ public   static partial class AiResponse
         var result = new AiResponseType<T>(rawResponse, JsonSerializer.Deserialize<T>(rawResponse));
         return result;
     }
-
-    public static async Task<AiResponseType<T?>> Create<T>(this HttpResponseMessage response)
-    {
-        response.EnsureSuccessStatusCode();
-        var responseBody = await response.Content.ReadAsStringAsync();
-        var ollamaResponse = JsonSerializer.Deserialize<OllamaResponse>(responseBody);
-        if (ollamaResponse == null)
-        {
-            return new AiResponseType<T?>(string.Empty, default);
-        }
-        else
-        {
-            return Create<T?>(ollamaResponse.Response);
-        }
-    }
+ 
 
 
 }
---
+++ Ollama/AiResponse.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using Ollama.Models;
+
+namespace Ollama
+{
+    public static partial class AiResponse
+    {
+  
+
+        public static async Task<AiResponseType<T?>> Create<T>(this HttpResponseMessage response)
+        {
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var ollamaResponse = JsonSerializer.Deserialize<OllamaResponse>(responseBody);
+            if (ollamaResponse == null)
+            {
+                return new AiResponseType<T?>(string.Empty, default);
+            }
+            else
+            {
+                return Create<T?>(ollamaResponse.Response);
+            }
+        }
+
+
+    }
+}

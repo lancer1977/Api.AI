0e83cdb229208fcf073807352cc92f0e6c3c88ca
chris <lancer1977@gmail.com>
2024-09-29 11:21:19 -0400

    dto fix

--- Interfaces/Models/AiResponseType.cs
+++ Interfaces/Models/AiResponseType.cs
@@ -3,6 +3,6 @@
 
 public record AiResponseType(string? Message)
 {
-    public bool IsSuccess => string.IsNullOrEmpty(Message);
+    public bool IsSuccess => !string.IsNullOrEmpty(Message);
     public List<int>? Context { get; set; }
 }
